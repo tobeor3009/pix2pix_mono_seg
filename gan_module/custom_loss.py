011ee914d48dfe21112cb23328d250de4708049c
tobeor3009 <tobeor3009@gmail.com>
2021-03-12 10:36:26 +0900

    refactor: fix constant naming isn't correct

--- gan_module/custom_loss.py
+++ gan_module/custom_loss.py
@@ -1,14 +1,14 @@
 import tensorflow as tf
 from tensorflow.keras import backend as K
 
-axis = [1, 2]
+AXIS = [1, 2]
 
 
 def f1_loss_for_training(y_true, y_pred):
 
-    tp = tf.math.reduce_sum(y_true * y_pred, axis=axis)
-    fp = tf.math.reduce_sum((1 - y_true) * y_pred, axis=axis)
-    fn = tf.math.reduce_sum(y_true * (1 - y_pred), axis=axis)
+    tp = tf.math.reduce_sum(y_true * y_pred, axis=AXIS)
+    fp = tf.math.reduce_sum((1 - y_true) * y_pred, axis=AXIS)
+    fn = tf.math.reduce_sum(y_true * (1 - y_pred), axis=AXIS)
 
     p = tp / (tp + fp + K.epsilon())  # Precision = tp / tp + fp
     r = tp / (tp + fn + K.epsilon())  # Recall = tp / (tp+fn)
@@ -23,9 +23,9 @@ def f1_score(y_true, y_pred):
     y_true = K.round(y_true)
     y_pred = K.round(y_pred)
 
-    tp = tf.math.reduce_sum(y_true * y_pred, axis=axis)
-    fp = tf.math.reduce_sum((1 - y_true) * y_pred, axis=axis)
-    fn = tf.math.reduce_sum(y_true * (1 - y_pred), axis=axis)
+    tp = tf.math.reduce_sum(y_true * y_pred, axis=AXIS)
+    fp = tf.math.reduce_sum((1 - y_true) * y_pred, axis=AXIS)
+    fn = tf.math.reduce_sum(y_true * (1 - y_pred), axis=AXIS)
 
     p = tp / (tp + fp + K.epsilon())  # Precision = tp / tp + fp
     r = tp / (tp + fn + K.epsilon())  # Recall = tp / (tp+fn)
@@ -39,10 +39,10 @@ def dice_loss_for_training(y_true, y_pred):
     # example input tensorsize y_true.shape = y_pred.shape = (4,512,512,1)
     # K means keras_background
     epsilon = K.epsilon()  # K.epsilon = 1e-7
-    intersection = tf.math.reduce_sum(y_true * y_pred, axis=axis)
+    intersection = tf.math.reduce_sum(y_true * y_pred, axis=AXIS)
     # add tensorwise, shape example : (4,512,512,1) => (4)
-    y_true_sum = tf.math.reduce_sum(y_true, axis=axis)
-    y_pred_sum = tf.math.reduce_sum(y_pred, axis=axis)  # add tensorwise
+    y_true_sum = tf.math.reduce_sum(y_true, axis=AXIS)
+    y_pred_sum = tf.math.reduce_sum(y_pred, axis=AXIS)  # add tensorwise
     # tensor shape = (4)
     dice_loss = (2. * intersection + epsilon) / (y_true_sum +
                                                  y_pred_sum + epsilon)  # 0 < loss <=1 shape : (4)
@@ -58,11 +58,11 @@ def dice_score(y_true, y_pred):
     # example input tensorsize y_true.shape = y_pred.shape = (4,512,512,1)
     # K means keras_background
     epsilon = K.epsilon()  # K.epsilon = 1e-7
-    intersection = tf.math.reduce_sum(y_true * y_pred, axis=axis)
+    intersection = tf.math.reduce_sum(y_true * y_pred, axis=AXIS)
     # add tensorwise, shape example : (4,512,512,1) => (4)
-    y_true_sum = tf.math.reduce_sum(y_true, axis=axis)
+    y_true_sum = tf.math.reduce_sum(y_true, axis=AXIS)
     # add tensorwise, shape example : (4,512,512,1) => (4)
-    y_pred_sum = tf.math.reduce_sum(y_pred, axis=axis)
+    y_pred_sum = tf.math.reduce_sum(y_pred, axis=AXIS)
 
     # tensor shape = (4)
     dice_loss = (2. * intersection + epsilon) / \
@@ -75,11 +75,11 @@ def jaccard_coef_loss_for_training(y_true, y_pred):
     # example input tensorsize y_true.shape = y_pred.shape = (4,512,512,1)
     # K means keras_background
     epsilon = K.epsilon()  # K.epsilon = 1e-7
-    intersection = tf.math.reduce_sum(y_true * y_pred, axis=axis)
+    intersection = tf.math.reduce_sum(y_true * y_pred, axis=AXIS)
     # add tensorwise, shape example : (4,512,512,1) => (4)
-    y_true_sum = tf.math.reduce_sum(y_true, axis=axis)
+    y_true_sum = tf.math.reduce_sum(y_true, axis=AXIS)
     # add tensorwise, shape example : (4,512,512,1) => (4)
-    y_pred_sum = tf.math.reduce_sum(y_pred, axis=axis)
+    y_pred_sum = tf.math.reduce_sum(y_pred, axis=AXIS)
     # tensor shape = (4)
     jaccard_coef = (intersection + epsilon) / \
         (y_true_sum + y_pred_sum - intersection + epsilon)
@@ -93,80 +93,13 @@ def jaccard_coef_score(y_true, y_pred):
     # example input tensorsize y_true.shape = y_pred.shape = (4,512,512,1)
     # K means keras_background
     epsilon = K.epsilon()  # K.epsilon = 1e-7
-    intersection = tf.math.reduce_sum(y_true * y_pred, axis=axis)
+    intersection = tf.math.reduce_sum(y_true * y_pred, axis=AXIS)
     # add tensorwise, shape example : (4,512,512,1) => (4)
-    y_true_sum = tf.math.reduce_sum(y_true, axis=axis)
+    y_true_sum = tf.math.reduce_sum(y_true, axis=AXIS)
     # add tensorwise, shape example : (4,512,512,1) => (4)
-    y_pred_sum = tf.math.reduce_sum(y_pred, axis=axis)
+    y_pred_sum = tf.math.reduce_sum(y_pred, axis=AXIS)
     # tensor shape = (4)
     jaccard_coef = (intersection + epsilon) / \
         (y_true_sum + y_pred_sum - intersection + epsilon)
 
     return K.mean(jaccard_coef)
-# def compute_msd(trajectory, t_step):
-#     diffs = trajectory- t_step
-#     sqdist = np.square(diffs).sum(axis=1)
-#     msds = sqdist.mean()
-#     msds_std = sqdist.std()
-
-#     return msds
-
-# def dice_coef(y_true, y_pred):
-#     smooth = 0.0001
-#     y_true_f = y_true.flatten()
-#     y_pred_f = y_pred.flatten()
-#     intersection = np.sum(y_true_f * y_pred_f)
-#     return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth) # 1>= loss >0
-
-# def jaccard_coef(y_true, y_pred):
-#     smooth = 0.0001
-#     y_true_f = y_true.flatten()
-#     y_pred_f = y_pred.flatten()
-#     intersection = np.sum(y_true_f * y_pred_f)
-#     return (intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) - intersection + smooth) # 1>= loss >0
-
-
-# def f1_loss_for_training(y_true, y_pred):
-
-#     tp = y_true * y_pred
-#     tp = K.sum(tp * y_pred) / (K.sum(y_pred) + K.epsilon())
-
-#     tn = (1-y_true) * (1-y_pred)
-#     tn = K.sum(tn * (1-y_pred)) / (K.sum(1-y_pred) + K.epsilon())
-
-#     fp = (1-y_true) * y_pred
-#     fp = K.sum(fp * y_pred) / (K.sum(y_pred)  + K.epsilon())
-
-#     fn = y_true * (1-y_pred)
-#     fn = K.sum(fn * (1-y_pred)) / (K.sum(1-y_pred) + K.epsilon())
-
-#     p = tp / (tp + fp + K.epsilon()) # Precision = tp / tp + fp
-#     r = tp / (tp + fn + K.epsilon()) # Recall = tp / (tp+fn)
-
-#     f1 = (2*p*r) / (p+r+K.epsilon())
-
-#     return 1 - f1
-
-# def f1_score(y_true, y_pred):
-
-#     y_true = K.round(y_true)
-#     y_pred = K.round(y_pred)
-
-#     tp = y_true * y_pred
-#     tp = K.sum(tp * y_pred) / (K.sum(y_pred) + K.epsilon())
-
-#     tn = (1-y_true) * (1-y_pred)
-#     tn = K.sum(tn * (1-y_pred)) / (K.sum(1-y_pred) + K.epsilon())
-
-#     fp = (1-y_true) * y_pred
-#     fp = K.sum(fp * y_pred) / (K.sum(y_pred)  + K.epsilon())
-
-#     fn = y_true * (1-y_pred)
-#     fn = K.sum(fn * (1-y_pred)) / (K.sum(1-y_pred) + K.epsilon())
-
-#     p = tp / (tp + fp + K.epsilon()) # Precision = tp / tp + fp
-#     r = tp / (tp + fn + K.epsilon()) # Recall = tp / (tp+fn)
-
-#     f1 = (2*p*r) / (p+r+K.epsilon())
-
-#     return f1
