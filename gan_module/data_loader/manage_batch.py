c808c84ed41d6cac6b526026041f41eb3835b8f0
tobeor3009 <tobeor3009@gmail.com>
2021-03-17 14:36:31 +0900

    refactor: use iterator when load batch

--- gan_module/data_loader/manage_batch.py
+++ gan_module/data_loader/manage_batch.py
@@ -1,20 +1,24 @@
 import threading
 from queue import Queue
 import tensorflow as tf
+import numpy as np
 
 
-class BatchQueueManager():
-
-    def __init__(self, train_class):
+class BatchQueueManager:
+    def __init__(self, train_class, batch_size, on_memory):
 
         self.train_class = train_class
+        self.batch_size = batch_size
+        self.on_memory = on_memory
         self.__batch_queue = {"train": Queue(), "valid": Queue()}
 
         self.__batch_setter = {
             "train": threading.Thread(
-                target=self.__batch_setter, args=("train",), daemon=True).start(),
+                target=self.__batch_setter, args=("train",), daemon=True
+            ).start(),
             "valid": threading.Thread(
-                target=self.__batch_setter, args=("valid",), daemon=True).start()
+                target=self.__batch_setter, args=("valid",), daemon=True
+            ).start(),
         }
 
     def get_batch(self, data_mode):
@@ -28,18 +32,43 @@ class BatchQueueManager():
 
     def __batch_setter(self, data_mode):
         while True:
-            self.__fill_queue(data_mode=data_mode)
+            self.__fill_queue_test(data_mode=data_mode)
 
     def __fill_queue(self, data_mode):
         batch_i = 0
-        while (batch_i + self.train_class.batch_size
-                <= self.train_class.data_loader.data_length[data_mode]):
+        while (
+            batch_i + self.train_class.batch_size
+            <= self.train_class.data_loader.data_length[data_mode]
+        ):
 
-            batch_index = self.train_class.loaded_data_index[data_mode][batch_i: batch_i +
-                                                                        self.train_class.batch_size]
+            batch_index = self.train_class.loaded_data_index[data_mode][
+                batch_i : batch_i + self.train_class.batch_size
+            ]
             batch_tuple = self.train_class.data_loader.get_data(
-                data_mode=data_mode, index=batch_index)
+                data_mode=data_mode, index=batch_index
+            )
 
             self.__batch_queue[data_mode].put(batch_tuple)
             self.__batch_queue[data_mode].join()
             batch_i += self.train_class.batch_size
+
+    def __fill_queue_test(self, data_mode):
+        batch_input_img = np.empty((self.batch_size, *self.train_class.input_img_shape))
+        batch_output_img = np.empty((self.batch_size, *self.train_class.output_img_shape))
+        if self.on_memory:
+            iter_object = enumerate(
+                zip(*self.train_class.data_loader.loaded_data_object[data_mode].values())
+            )
+        else:
+            iter_object = enumerate(self.train_class.data_loader.loaded_data_object[data_mode])
+        for index, (input_img, output_img) in iter_object:
+            if index > 0 and index % self.batch_size == 0:
+                batch_tuple = (
+                    batch_input_img.copy(),
+                    batch_output_img.copy(),
+                )
+                self.__batch_queue[data_mode].put(batch_tuple)
+                self.__batch_queue[data_mode].join()
+            batch_input_img[index % self.batch_size] = input_img
+            batch_output_img[index % self.batch_size] = output_img
+
--- gan_module/data_loader/medical_segmentation_data_loader.py
+++ gan_module/data_loader/medical_segmentation_data_loader.py
@@ -18,19 +18,19 @@ class DataLoader:
         dataset_path = os.path.join(current_path, "datasets", dataset_name)
         image_data_path_regexp = {
             "train": os.path.join(dataset_path, "train", "image", "*"),
-            "valid": os.path.join(dataset_path, "valid", "image", "*")
+            "valid": os.path.join(dataset_path, "valid", "image", "*"),
         }
         mask_data_path_regexp = {
             "train": os.path.join(dataset_path, "train", "mask", "*"),
-            "valid": os.path.join(dataset_path, "valid", "mask", "*")
+            "valid": os.path.join(dataset_path, "valid", "mask", "*"),
         }
         self.image_file_paths = {
             "train": np.array(glob(image_data_path_regexp["train"])),
-            "valid": np.array(glob(image_data_path_regexp["valid"]))
+            "valid": np.array(glob(image_data_path_regexp["valid"])),
         }
         self.mask_file_paths = {
             "train": np.array(glob(mask_data_path_regexp["train"])),
-            "valid": np.array(glob(mask_data_path_regexp["valid"]))
+            "valid": np.array(glob(mask_data_path_regexp["valid"])),
         }
         if code_test:
             self.image_file_paths["train"] = self.image_file_paths["train"][:TEST_DATA_NUM]
@@ -39,34 +39,57 @@ class DataLoader:
             self.mask_file_paths["valid"] = self.mask_file_paths["valid"][:TEST_DATA_NUM]
         self.data_length = {
             "train": len(self.image_file_paths["train"]),
-            "valid": len(self.image_file_paths["valid"])
+            "valid": len(self.image_file_paths["valid"]),
         }
         self.data_index = {
             "train": np.arange(self.data_length["train"]),
-            "valid": np.arange(self.data_length["valid"])
+            "valid": np.arange(self.data_length["valid"]),
         }
         # -----------------
-        #  load File if on_memory
+        #  load File Object (array or generator)
         # -----------------
-        if self.on_memory:
-            self.loaded_data = self.__load_whole_data()
+        self.loaded_data_object = self.__get_data_object()
 
     def get_data(self, data_mode, index=None):
         if self.on_memory:
             if index is None:
-                data_tuple = self.loaded_data[data_mode]
+                data_dict = self.loaded_data[data_mode]
             else:
-                data_tuple = (self.loaded_data[data_mode][0][index],
-                              self.loaded_data[data_mode][1][index])
+                data_dict = (
+                    self.loaded_data_object[data_mode]["input"][index],
+                    self.loaded_data_object[data_mode]["output"][index],
+                )
         else:
-            data_tuple = self.__get_processed_imgs(
-                data_mode=data_mode, index=index)
-        return data_tuple
+            data_dict = self.__get_processed_imgs(data_mode=data_mode, index=index)
+        return data_dict
 
-    def __load_whole_data(self):
-        tuple_train_data = self.__get_processed_imgs(data_mode="train")
-        tuple_valid_data = self.__get_processed_imgs(data_mode="valid")
-        return {"train": tuple_train_data, "valid": tuple_valid_data}
+    def shuffle_train_imgs(self):
+        np.random.shuffle(self.data_index["train"])
+        if self.on_memory:
+            self.loaded_data_object["train"]["input"] = self.loaded_data_object["train"]["input"][
+                self.data_index["train"]
+            ]
+            self.loaded_data_object["train"]["output"] = self.loaded_data_object["train"]["output"][
+                self.data_index["train"]
+            ]
+        else:
+            self.image_file_paths["train"] = self.image_file_paths["train"][
+                self.data_index["train"]
+            ]
+
+    def __get_data_object(self):
+        if self.on_memory:
+            dict_train_data = self.__get_processed_imgs(data_mode="train")
+            dict_valid_data = self.__get_processed_imgs(data_mode="valid")
+            return {"train": dict_train_data, "valid": dict_valid_data}
+        else:
+            generator_train_data = self.__get_generator_of_processed_imgs(data_mode="train")
+            generator_valid_data = self.__get_generator_of_processed_imgs(data_mode="valid")
+            return {
+                "train": generator_train_data,
+                "valid": generator_valid_data,
+            }
+
     # separate imgs and make png data range(-255,255) to (-1,1)
 
     def __get_processed_imgs(self, data_mode, index=None):
@@ -77,31 +100,45 @@ class DataLoader:
         else:
             image_paths = image_paths[index]
             mask_paths = mask_paths[index]
-        image_list_shape = (len(image_paths),
-                            *self.config_dict["img_shape"], self.config_dict["input_channels"])
-        mask_list_shape = (len(mask_paths),
-                           *self.config_dict["img_shape"], self.config_dict["output_channels"])
-        image_list = np.empty(image_list_shape, dtype=np.float32)
-        mask_list = np.empty(mask_list_shape, dtype=np.float32)
-        for index, (image_path, mask_path) in enumerate(zip(image_paths, mask_paths)):
-            image = imread(image_path, channel='bgr')
+        input_array_shape = (
+            len(image_paths),
+            *self.config_dict["img_shape"],
+            self.config_dict["input_channels"],
+        )
+        output_array_shape = (
+            len(mask_paths),
+            *self.config_dict["img_shape"],
+            self.config_dict["output_channels"],
+        )
+        input_image_list = np.empty(input_array_shape, dtype=np.float32)
+        output_image_list = np.empty(output_array_shape, dtype=np.float32)
+        for img_index, (image_path, mask_path) in enumerate(zip(image_paths, mask_paths)):
+            image = imread(image_path, channel="bgr")
             mask = imread(mask_path)
-            image_list[index] = image
-            mask_list[index] = mask
-        image_list = image_list / 127.5 - 1.0
-        mask_list = np.round(mask_list / 255)
-        return image_list, mask_list
+            input_image_list[img_index] = image
+            output_image_list[img_index] = mask
+        input_image_list = input_image_list / 127.5 - 1.0
+        output_image_list = np.round(output_image_list / 255)
+        return {"input": input_image_list, "output": output_image_list}
+
+    def __get_generator_of_processed_imgs(self, data_mode):
+        image_paths = self.image_file_paths[data_mode]
+        mask_paths = self.mask_file_paths[data_mode]
+        for (image_path, mask_path) in enumerate(zip(image_paths, mask_paths)):
+            input_image = imread(image_path, channel="bgr")
+            output_image = imread(mask_path)
+            input_image = input_image / 127.5 - 1.0
+            output_image = np.round(output_image / 255)
+            yield input_image, output_image
 
 
 def imread(path, channel=None):
     image_byte_stream = open(path.encode("utf-8"), "rb")
     image_byte_array = bytearray(image_byte_stream.read())
     image_numpy_array = np.asarray(image_byte_array, dtype=np.uint8)
-    image_numpy_array = cv2.imdecode(
-        image_numpy_array, cv2.IMREAD_UNCHANGED)
-    if channel == 'bgr':
-        image_numpy_array = cv2.cvtColor(
-            image_numpy_array, cv2.COLOR_BGR2RGB)
+    image_numpy_array = cv2.imdecode(image_numpy_array, cv2.IMREAD_UNCHANGED)
+    if channel == "bgr":
+        image_numpy_array = cv2.cvtColor(image_numpy_array, cv2.COLOR_BGR2RGB)
     if len(image_numpy_array.shape) == 2:
         image_numpy_array = np.expand_dims(image_numpy_array, axis=-1)
     return image_numpy_array
@@ -113,11 +150,11 @@ class DataLoaderInfo:
         dataset_path = os.path.join(current_path, "datasets", dataset_name)
         data_path_regexp = {
             "image": os.path.join(dataset_path, data_mode, "image", "*"),
-            "mask": os.path.join(dataset_path, data_mode, "mask", "*")
+            "mask": os.path.join(dataset_path, data_mode, "mask", "*"),
         }
         self.image_file_paths = {
             "image": np.array(glob(data_path_regexp["image"])),
-            "mask": np.array(glob(data_path_regexp["mask"]))
+            "mask": np.array(glob(data_path_regexp["mask"])),
         }
         self.data_length = len(self.image_file_paths["image"]["train"])
         self.data_index = np.arange(self.data_length)
