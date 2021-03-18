d982bc17f838d54f19dff934731c681aa67a3e5f
tobeor3009 <tobeor3009@gmail.com>
2021-03-18 13:15:29 +0900

    refactor: refactor logger and change logfile location

--- gan_module/util/logger.py
+++ gan_module/util/logger.py
@@ -1,20 +1,12 @@
 import logging
+from datetime import datetime
 
 
 class TrainLogger:
-    def __init__(self, name="train"):
-        logger = logging.getLogger(name)
-        logger.setLevel(logging.INFO)
-        formatter = logging.Formatter(
-            "{\n%(asctime)s - %(name)s - %(levelname)s - \n%(message)s\n}"
-        )
-        stream_handler = logging.StreamHandler()
-        file_handler = logging.FileHandler(f"./{name}.log")
-        stream_handler.setFormatter(formatter)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(stream_handler)
-        logger.addHandler(file_handler)
-        self.logger = logger
+    def __init__(self):
+        self.logger = None
+        self.__define_logger()
+
         self.log_info_dict = {
             "epoch": "",
             "discriminator_acces": "",
@@ -36,4 +28,19 @@ class TrainLogger:
         for key, value in self.log_info_dict.items():
             message.append(f"{key} : {value}")
         self.logger.info("\n".join(message))
+
+    def __define_logger(self):
+
+        logger = logging.getLogger("train")
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            "{\n%(asctime)s - %(name)s - %(levelname)s - \n%(message)s\n}"
+        )
+        stream_handler = logging.StreamHandler()
+        file_handler = logging.FileHandler(f"./log/train_{datetime.now()}.log")
+        stream_handler.setFormatter(formatter)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+        logger.addHandler(file_handler)
+        self.logger = logger
 
