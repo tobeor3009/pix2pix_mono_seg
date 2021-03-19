88f0493153c447300b2697ccde020e959ce425be
tobeor3009 <tobeor3009@gmail.com>
2021-03-19 11:32:27 +0900

    chore: change learning_rate schedule

--- gan_module/util/manage_learning_rate.py
+++ gan_module/util/manage_learning_rate.py
@@ -1,22 +1,24 @@
-def learning_rate_scheduler(learning_rate, epoch, schedule_list=None, exponent=0.2, warm_up=True, warm_up_epoch=10):
+def learning_rate_scheduler(
+    learning_rate, epoch, schedule_list=None, exponent=0.2, warm_up=True, warm_up_epoch=20
+):
     step = 0
     if warm_up and epoch < warm_up_epoch:
         new_learning_rate = learning_rate * ((epoch + 1) / warm_up_epoch)
     else:
         if schedule_list is None:
-            schedule_list = [30, 100, 175, 250, 325]
+            schedule_list = [75, 175, 250, 325, 400]
         for step, target_epoch in enumerate(schedule_list):
             if target_epoch > epoch:
                 break
             else:
                 continue
-        new_learning_rate = learning_rate * (exponent**(step))
+        new_learning_rate = learning_rate * (exponent ** (step))
 
     return new_learning_rate
 
 
 def poly_learning_rate_scheduler(learning_rate, epoch, max_epoch=300, exponent=0.9):
     # poly_lr
-    new_learning_rate = learning_rate * (1 - epoch / max_epoch)**exponent
+    new_learning_rate = learning_rate * (1 - epoch / max_epoch) ** exponent
 
     return new_learning_rate
