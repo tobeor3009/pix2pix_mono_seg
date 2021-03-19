203b59a816293fb2411bfc661a8ff58321a77bf2
tobeor3009 <tobeor3009@gmail.com>
2021-03-19 11:33:05 +0900

    feat: add date info to logfile name

--- gan_module/util/logger.py
+++ gan_module/util/logger.py
@@ -1,6 +1,8 @@
 import logging
 from datetime import datetime
 
+DATE_FORMAT = "%Y-%m-%d %H.%M"
+
 
 class TrainLogger:
     def __init__(self):
@@ -8,8 +10,8 @@ class TrainLogger:
         self.__define_logger()
 
         self.log_info_dict = {
-            "epoch": "",
-            "discriminator_acces": "",
+            "Epoch": "",
+            "Discriminator_acces": "",
             "Mean generator loss": "",
             "Max generator loss": "",
             "Min generator loss": "",
@@ -19,13 +21,31 @@ class TrainLogger:
             "Elapsed_time": "",
         }
 
-    def get_log(self, *args):
+        self.history_info_dict = {
+            "Epoch": [],
+            "Discriminator_acces": [],
+            "Generator loss": [],
+            "Train_f1_loss": [],
+            "Valid_f1_loss": [],
+        }
+
+    def write_log(self, *args):
         # setting args to log_info_dict
         for key, value in zip(self.log_info_dict, args):
             self.log_info_dict[key] = value
 
         message = []
         for key, value in self.log_info_dict.items():
+            message.append(f"{key} : {value}")
+        self.logger.info("\n".join(message))
+
+    def add_histroy(self, *args):
+        # setting args to history_info_dict
+        for key, value in zip(self.history_info_dict, args):
+            self.history_info_dict[key] = value
+
+        message = []
+        for key, value in self.history_info_dict.items():
             message.append(f"{key} : {value}")
         self.logger.info("\n".join(message))
 
@@ -37,10 +57,10 @@ class TrainLogger:
             "{\n%(asctime)s - %(name)s - %(levelname)s - \n%(message)s\n}"
         )
         stream_handler = logging.StreamHandler()
-        file_handler = logging.FileHandler(f"./log/train_{datetime.now()}.log")
+        current_time = datetime.now().strftime(DATE_FORMAT)
+        file_handler = logging.FileHandler(f"./log/train_{current_time}.log")
         stream_handler.setFormatter(formatter)
         file_handler.setFormatter(formatter)
         logger.addHandler(stream_handler)
         logger.addHandler(file_handler)
         self.logger = logger
-
