138c25563046bf5a3a10ccb9dfef4e54c6a9c290
tobeor3009 <tobeor3009@gmail.com>
2021-04-09 08:00:48 +0900

    improve: progressbar show more info

--- gan_module/util/display_progress.py
+++ gan_module/util/display_progress.py
@@ -3,7 +3,15 @@ import progressbar
 
 class ProgressBar:
     def __init__(self, total_length):
-        self.bar = progressbar.ProgressBar(maxval=total_length).start()
+        widgets = [
+            ' [', progressbar.Counter(format='%(value)02d/%(max_val)d'), '] ',
+            progressbar.Bar(),
+            ' (', progressbar.ETA(), ') ',
+        ]
+        self.bar = progressbar.ProgressBar(
+            widgets=widgets,
+            max_val=total_length
+        ).start()
 
     def update(self, batch_i):
         self.bar.update(batch_i)
