aeee384362e7710d121269c295380e09eb2246a5
tobeor3009 <tobeor3009@gmail.com>
2021-04-09 08:05:04 +0900

    chore: change default learning epoch schedule

--- gan_module/util/manage_learning_rate.py
+++ gan_module/util/manage_learning_rate.py
@@ -1,12 +1,12 @@
 def learning_rate_scheduler(
-    learning_rate, epoch, schedule_list=None, exponent=0.2, warm_up=True, warm_up_epoch=20
+    learning_rate, epoch, schedule_list=None, exponent=0.1, warm_up=True, warm_up_epoch=20
 ):
     step = 0
     if warm_up and epoch < warm_up_epoch:
         new_learning_rate = learning_rate * ((epoch + 1) / warm_up_epoch)
     else:
         if schedule_list is None:
-            schedule_list = [75, 175, 250, 325, 400]
+            schedule_list = [25, 100, 175, 250, 325]
         for step, target_epoch in enumerate(schedule_list):
             if target_epoch > epoch:
                 break
