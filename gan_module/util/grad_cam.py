151954d1b073093d54a3add54f87d0381fc35efe
tobeor3009 <tobeor3009@gmail.com>
2021-06-15 11:40:00 +0900

    daily commit

--- gan_module/util/grad_cam.py
+++ gan_module/util/grad_cam.py
@@ -1,22 +1,117 @@
-from tensorflow import keras as tf_keras
+import cv2
+import os
 import numpy as np
+
+
+def imread(img_path, channel=None):
+    img_byte_stream = open(img_path.encode("utf-8"), "rb")
+    img_byte_array = bytearray(img_byte_stream.read())
+    img_numpy_array = np.asarray(img_byte_array, dtype=np.uint8)
+    img_numpy_array = cv2.imdecode(
+        img_numpy_array, cv2.IMREAD_UNCHANGED)
+    if channel == "rgb":
+        img_numpy_array = cv2.cvtColor(
+            img_numpy_array, cv2.COLOR_BGR2RGB)
+    if len(img_numpy_array.shape) == 2:
+        img_numpy_array = np.expand_dims(img_numpy_array, axis=-1)
+    return img_numpy_array
+
+
+from tensorflow import keras
+import matplotlib.cm as cm
 
 BASE_DTYPE = "float32"
 
-"""
-change image to tensor shape (1, height, width, channel)
-img_path: str
-size: tuple or list
-dtype: str or tf dtype 
-"""
-def get_img_array(img_path, size, dtype=BASE_DTYPE):
-    # `img` is a PIL image of size 299x299
-    img = tf_keras.preprocessing.image.load_img(img_path, target_size=size)
-    # `array` is a float32 Numpy array of shape (299, 299, 3)
-    array = tf_keras.preprocessing.image.img_to_array(img, dtype=dtype)
-    # We add a dimension to transform our array into a "batch"
-    # of size (1, 299, 299, 3)
-    array = np.expand_dims(array, axis=0)
-    return array
+
+def get_last_conv_name(model):
+    layer_names = [layer.name for layer in model.layers]
+    conv_layer_name = [
+        layer_name for layer_name in layer_names if layer_name.find("conv") >= 0]
+    last_conv_name = conv_layer_name[-1]
+
+    return last_conv_name
 
 
+def get_img_array(img_path, channel="rgb", dtype=BASE_DTYPE):
+
+    img_array = imread(img_path, channel="rgb")
+    img_array = np.expand_dims(img_array, axis=0)
+
+    return img_array
+
+
+def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
+    # Model input: model.input
+    # Model output: [last_conv_layer_outputs, model.outputs]
+    grad_model = keras.models.Model(
+        model.input, [model.get_layer(
+            last_conv_layer_name).output, model.output]
+    )
+
+    # compute
+    # 1. last_conv_layer_output
+    # 2. class_channel value in prediction
+    with tf.GradientTape() as tape:
+        last_conv_layer_output, preds = grad_model(img_array)
+        if pred_index is None:
+            pred_index = tf.argmax(preds[0])
+        class_channel = preds[:, pred_index]
+
+    # compute gradient
+    grads = tape.gradient(class_channel, last_conv_layer_output)
+
+    # compute gradient channel mean
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    # compute matmul(last_conv_layer_output, pooled_grads)
+    last_conv_layer_output = last_conv_layer_output[0]
+    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+
+    # For visualization purpose, we will also normalize the heatmap between 0 & 1
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    return heatmap.numpy()
+
+
+def save_and_display_gradcam(img_path, heatmap, cam_path="cam.jpg", alpha=0.4):
+    # Load the original image
+    img = keras.preprocessing.image.load_img(img_path)
+    img = keras.preprocessing.image.img_to_array(img)
+
+    # Rescale heatmap to a range 0-255
+    heatmap = np.uint8(255 * heatmap)
+
+    # Use jet colormap to colorize heatmap
+    jet = cm.get_cmap("jet")
+
+    # Use RGB values of the colormap
+    jet_colors = jet(np.arange(256))[:, :3]
+    jet_heatmap = jet_colors[heatmap]
+
+    # Create an image with RGB colorized heatmap
+    jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
+    jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+    jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
+
+    # Superimpose the heatmap on original image
+    superimposed_img = jet_heatmap * alpha + img
+    superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
+
+    # Save the superimposed image
+    superimposed_img.save(cam_path)
+
+    # Display Grad CAM
+    display(Image.open(cam_path))
+
+
+def decode_classify_predictions(pred_array, index_to_label_dict):
+    predicted_index = np.sum(np.argmax(preds, axis=1))
+    predicted = index_to_label_dict[predicted_index]
+
+    return predicted
+
+
+def decode_binary_classify_predictions(pred_array, index_to_label_dict):
+    predicted = np.round(pred_array)
+    predicted = predicted.flatten()[0]
+    return predicted
